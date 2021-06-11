effe03e73604850bf24212c9e07c8f4e394fffc8
Gordon Liew <gordonliewgl@gmail.com>
2021-06-11 17:52:52 +0800

    Added functionality for useVideos custom hook

--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -1,8 +1,24 @@
 import {useEffect, useState} from 'react';
 import YouTube from '../apis/youtube.js';
 
-const useVideos = () => {
+const useVideos = (defaultSearchTerm) => {
+	const [videos, setVideos] = useState([]);
 
+	const search = async (searchTerm) => {
+		const res = await YouTube.get('/search', {
+			params: {q: searchTerm}
+		})
+
+		// Brings up a list of 5 videos on the right hand side and
+		// automatically plays the first video in the left side
+		setVideos(res.data.items);
+	};
+
+	useEffect(() => {
+		search(defaultSearchTerm);
+	}, [defaultSearchTerm])
+
+	return [videos, search];
 };
 
 export default useVideos;
